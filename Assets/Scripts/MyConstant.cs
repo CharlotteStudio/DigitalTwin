d62cf48c1ec11827be61dff0ac2c3fbd53668eb3
Charlotte Studio <charlottexprojects@gmail.com>
2023-06-03 23:36:30 +0800

    add a login page & UI

---
+++ Assets/Scripts/MyConstant.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyConstant
+{
+    public const string UserName = "testing@gmail.com";
+    public const string Password = "123456";
+    public const string MainScene = "MainScene";
+}
