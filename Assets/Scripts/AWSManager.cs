094eaa41e93549b69d7ff9ed31d766c81a38f1ad
Charlotte Studio <charlottexprojects@gmail.com>
2023-06-12 18:28:39 +0800

    wip add AWS manager

--- Assets/Scripts/AWSManager.cs
+++ Assets/Scripts/AWSManager.cs
@@ -1,18 +1,99 @@
+using System;
+using System.IO;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using SquareBeam.AWS.Lambda;
+using SquareBeam.AWS.Lambda.Models;
 
 public class AWSManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private LambdaAPI _LambdaClient;
+    
+    void Awake()
     {
-        
+#if DEVELOPMENT
+        var credentialsFile = "lambda.json";
+        var pathToCredentials = Path.Combine(Directory.GetParent(Application.dataPath).Parent.FullName, "credentials", credentialsFile);
+        credentials = JsonUtility.FromJson<LambdaCredentials>(File.ReadAllText(pathToCredentials));
+#endif
+
+        SetUpLambdaClient();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetUpLambdaClient()
     {
+        _LambdaClient.Setup(
+            MyConstant.AWSService.AccessKey,
+            MyConstant.AWSService.SecretKey,
+            MyConstant.AWSService.Region);
+    }
+
+    public void TryGetDeviceCurrentValue()
+    {
+        StartCoroutine(InvokeLambda(MyConstant.AWSService.LambdaFunction.GetDeviceCurrentValue));
+    }
+    
+    private IEnumerator InvokeLambda(string lambdaFunctionName)
+    {
+        string testFile = "lambda_payload.json";
+        string payload = File.ReadAllText(
+            Path.Combine(Application.dataPath, "AWS", "Services", "Lambda", "Example", "TestFiles", testFile));
         
+        var request = new LambdaRequest(lambdaFunctionName, payload)
+        {
+            onDone = OnDone
+        };
+        
+        void OnDone(LambdaResponse baseResponse)
+        {
+            var response = (LambdaResponse)baseResponse;
+
+            if (response.Success)
+            {
+                $"Success: Response is {response.DownloadHandler.text}".DebugLog();
+            }
+            else
+            {
+                var exceptionType = response.Exception.GetType();
+                $"Failure: {exceptionType} was thrown!".DebugLogError();
+
+                if (exceptionType == typeof(LambdaException))
+                {
+                    var exception = (LambdaException)response.Exception;
+
+                    exception.ToString().DebugLogError();
+                }
+                else if (exceptionType == typeof(Exception))
+                {
+                    Debug.LogException(response.Exception);
+                }
+                else
+                    $"Unsupported exception type {exceptionType}".DebugLogError();
+            }
+        }
+        yield return _LambdaClient.LambdaInvoke(request);
     }
 }
+
+#region Editor Function
+
+#if UNITY_EDITOR
+
+[UnityEditor.CustomEditor(typeof(AWSManager))]
+public class AWSManagerEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var awsManager = (AWSManager) target;
+
+        if (GUILayout.Button("Try Get Device Current Value"))
+        {
+            awsManager.SetUpLambdaClient();
+            awsManager.TryGetDeviceCurrentValue();
+        }
+    }
+}
+#endif
+
+#endregion
--- Assets/Scripts/MyConstant.cs
+++ Assets/Scripts/MyConstant.cs
@@ -1,10 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public static class MyConstant
 {
     public const string UserName = "testing@gmail.com";
     public const string Password = "123456";
     public const string MainScene = "MainScene";
+
+    public static class AWSService
+    {
+        public const string AccessKey = "AKIA2LAI6V2UE2ASRLBX";
+        public const string SecretKey = "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";
+        public const string Region = "ap-northeast-1";
+        
+        public static class LambdaFunction
+        {
+            public const string GetDeviceCurrentValue = "IBSP_GetDeviceCurrentValue";
+            public const string GetDeviceValue = "";
+        }
+    }
 }
