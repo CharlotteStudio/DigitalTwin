a2a8e1afafef06b117560fc90d8255b9812afafa
Charlotte Studio <charlottexprojects@gmail.com>
2023-06-15 01:00:26 +0800

    main scene add device demo

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -8,24 +6,43 @@ public class UIManager : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] private Button editorButton;
+    [SerializeField] private Button settingButton;
 
     [Header("Dialog")]
     [SerializeField] private GameObject editorBlock;
+    [SerializeField] private GameObject settingBlock;
+
     
     [Space(10)]
     [Header("Events")]
     public UnityEvent OnClickEditorButtonEvents;
+    public UnityEvent OnClickSettingButtonEvents;
+
 
     private void Start()
     {
         editorButton.gameObject.SetActive(true);
+        settingButton.gameObject.SetActive(true);
         editorBlock.SetActive(false);
+        settingBlock.SetActive(false);
         editorButton.onClick.AddListener(EnableEditorBlock);
+        settingButton.onClick.AddListener(EnableSettingBlock);
     }
 
     private void EnableEditorBlock()
     {
+        editorButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(false);
+        editorBlock.SetActive(true);
         OnClickEditorButtonEvents?.Invoke();
         // TODO : Change Camera
+    }
+    
+    private void EnableSettingBlock()
+    {
+        editorButton.gameObject.SetActive(false);
+        settingButton.gameObject.SetActive(false);
+        settingBlock.SetActive(true);
+        OnClickSettingButtonEvents?.Invoke();
     }
 }
