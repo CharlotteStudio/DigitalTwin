1d7de1216422b43f859f1ea1ec95c9a9e027b2ea
Charlotte Studio <charlottexprojects@gmail.com>
2023-06-18 23:21:10 +0800

    add save device position

--- Assets/Scripts/Manager/EditorModeManager.cs
+++ Assets/Scripts/Manager/EditorModeManager.cs
@@ -26,7 +26,17 @@ public class EditorModeManager : MonoBehaviour
     {
         createPlaneButton.onClick.AddListener(CreatePlaneObject);
         saveButton.onClick.AddListener(OnClickSaveButton);
-        ReadUserSave();
+        ReadFarmlandPositionSave();
+        
+        if (DeviceManager.Instance != null)
+            DeviceManager.Instance.OnSpawnedDeviceEvent += ReadDevicePositionSave;
+    }
+    
+    private void OnClickSaveButton()
+    {
+        WriteFarmlandSave();
+        WriteDeviceSave();
+        OnClickSaveButtonEvents?.Invoke();
     }
 
     private void CreatePlaneObject()
@@ -39,24 +49,47 @@ public class EditorModeManager : MonoBehaviour
         _planeList.Add(Instantiate(fieldGroup_1, vector3, Quaternion.identity, farmingRoot));
     }
 
-    private void ReadUserSave()
+    #region Read Write Save
+    
+    private void ReadFarmlandPositionSave()
     {
-        var vectorList = SaveManager.Instance.TryGetPositionSave();
+        var vectorList = SaveManager.Instance.TryGetFarmlandPositionSave();
+        
+        if (vectorList.Equals("")) return;
+        
         vectorList.DebugLog();
         
         foreach (var pos in vectorList)
         {
             CreatePlaneObject(pos);
         }
+        
+        "Completed Set up farmland position".DebugLog();
     }
-
-    private void OnClickSaveButton()
+    
+    private void ReadDevicePositionSave()
     {
-        WriteSave();
-        OnClickSaveButtonEvents?.Invoke();
+        var deviceDict = SaveManager.Instance.TryGetDevicePositionSave();
+        
+        if (deviceDict == null || deviceDict.Count == 0) return;
+
+        deviceDict.DebugLog();
+
+        if (DeviceManager.Instance == null)
+        {
+            "Can not find DeviceManager".DebugLogWarning();
+            return;
+        }
+
+        foreach (var pair in deviceDict)
+        {
+            DeviceManager.Instance.SetDevicePosition(pair.Key, pair.Value);
+        }
+        
+        "Completed Set up device position".DebugLog();
     }
 
-    private void WriteSave()
+    private void WriteFarmlandSave()
     {
         if (_planeList == null || _planeList.Count <= 0) return;
         
@@ -64,9 +97,25 @@ public class EditorModeManager : MonoBehaviour
         foreach(var plane in _planeList)
         {
             var pos = plane.transform.position;
-            str += $"{pos.x.ToString("F")},{pos.y.ToString("F")},{pos.z.ToString("F")}:";
+            str += $"{pos.x:F},{pos.y:F},{pos.z:F}:";
         }
         str.DebugLog();
-        SaveManager.Instance.SaveStringData("PlanePosition", str);
+        SaveManager.Instance.SaveStringData(MyConstant.SaveKey.FarmlandPosition, str);
     }
+
+    private void WriteDeviceSave()
+    {
+        if (DeviceManager.Instance == null || DeviceManager.Instance.deviceList.Count == 0) return;
+        
+        string str = "";
+        foreach(var deviceBase in DeviceManager.Instance.deviceList)
+        {
+            var pos = deviceBase.transform.position;
+            str += $"{deviceBase.mac_Address},{pos.x:F},{pos.y:F},{pos.z:F}*";
+        }
+        str.DebugLog();
+        SaveManager.Instance.SaveStringData(MyConstant.SaveKey.DevicePosition, str);
+    }
+    
+    #endregion
 }
--- Assets/Scripts/Manager/SaveManager.cs
+++ Assets/Scripts/Manager/SaveManager.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class SaveManager : ManagerBase<SaveManager>
 {
-    public List<Vector3> TryGetPositionSave()
+    public List<Vector3> TryGetFarmlandPositionSave()
     {
         List<Vector3> vectors = new List<Vector3>();
 
-        if (PlayerPrefs.HasKey("PlanePosition"))
+        if (PlayerPrefs.HasKey(MyConstant.SaveKey.FarmlandPosition))
         {
-
-            string posString = PlayerPrefs.GetString("PlanePosition");
+            string posString = PlayerPrefs.GetString(MyConstant.SaveKey.FarmlandPosition);
 
             string[] splitPos = posString.Split(':');
 
@@ -28,9 +27,59 @@ public class SaveManager : ManagerBase<SaveManager>
         return vectors;
     }
 
+    public Dictionary<string, Vector3> TryGetDevicePositionSave()
+    {
+        Dictionary<string /*mac address*/, Vector3 /*position*/> dict = new Dictionary<string, Vector3>();
+        
+        if (PlayerPrefs.HasKey(MyConstant.SaveKey.DevicePosition))
+        {
+            string posString = PlayerPrefs.GetString(MyConstant.SaveKey.DevicePosition);
+
+            string[] splitPos = posString.Split('*');
+
+            foreach(var pos in splitPos)
+            {
+                string[] vector = pos.Split(',');
+                
+                if (vector.Length < 4) continue;
+
+                Vector3 position = new Vector3(float.Parse(vector[1]), float.Parse(vector[2]), float.Parse(vector[3]));
+                dict.Add(vector[0], position);
+            }
+        }
+        return dict;
+    }
+
     public void SaveStringData(string key, string content)
     {
         PlayerPrefs.SetString(key, content);
         PlayerPrefs.Save(); 
     }
+
+    public void DeletedSave() => PlayerPrefs.DeleteAll();
+    
 }
+
+#region Editor Function
+
+#if UNITY_EDITOR
+
+[UnityEditor.CustomEditor(typeof(SaveManager))]
+public class SaveManagerEditor : UnityEditor.Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        var saveManager = (SaveManager) target;
+
+        GUILayout.Space(10);
+        GUILayout.Label("Editor Function:");
+        if (GUILayout.Button("Remove All Save"))
+        {
+            saveManager.DeletedSave();
+        }
+    }
+}
+#endif
+
+#endregion
