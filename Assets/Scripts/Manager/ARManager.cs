4752dcf12e1b41522ceaa3b35cb6ff7f2964b1c1
Charlotte Studio <charlottexprojects@gmail.com>
2023-07-09 22:01:00 +0800

    update AR Scene

--- Assets/Scripts/Manager/ARManager.cs
+++ Assets/Scripts/Manager/ARManager.cs
@@ -4,11 +4,23 @@ using UnityEngine.UI;
 
 public class ARManager : ManagerBase<ARManager>
 {
+    [Header("UI")]
     [SerializeField] private Button _backButton;
 
+    [Header("Object")]
+    [SerializeField] private Transform _spawnDeivceTransform;
+    [SerializeField] private Transform _targetARTransform;
+        
     private void Start()
     {
         _backButton.onClick.AddListener(
             ()=> SceneManager.LoadScene(MyConstant.MainScene));
+    }
+
+    public void SetDeviceObjectToARObject()
+    {
+        _spawnDeivceTransform.parent = _targetARTransform;
+        _spawnDeivceTransform.transform.localPosition = Vector3.zero;
+        _spawnDeivceTransform.transform.localRotation = Quaternion.identity;
     }
 }
