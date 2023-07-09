23d46e3fca8a3a08e0409c5244c2d3452fd28805
Charlotte Studio <charlottexprojects@gmail.com>
2023-07-09 21:58:43 +0800

    add touch zoom

---
+++ Assets/Scripts/Camera/TouchZoomSize.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+namespace TouchSample
+{
+    public class TouchZoomSize : MonoBehaviour
+    {
+        [SerializeField] private float _zoomSpeed = 0.05f;
+
+        private Vector2 _oldPost1;
+        private Vector2 _oldPost2;
+        
+        private void Update()
+        {
+            if (Input.touchCount != 2) return ;
+            if (Input.GetTouch(0).phase != TouchPhase.Moved || Input.GetTouch(1).phase != TouchPhase.Moved) return;
+            
+            Vector2 temPos1 = Input.GetTouch(0).position;
+            Vector2 temPos2 = Input.GetTouch(1).position;
+
+            float oldScale = transform.localScale.x;
+            float newScale;
+            if (isEnLarge(_oldPost1, _oldPost2, temPos1, temPos2))
+                newScale = oldScale * (1 + _zoomSpeed);
+            else
+                newScale = oldScale / (1 + _zoomSpeed);
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            _oldPost1 = temPos1;
+            _oldPost2 = temPos2;
+        }
+        
+        private bool isEnLarge(Vector2 oP1, Vector2 oP2, Vector2 nP1, Vector2 nP2)
+        {
+            float length1 = Mathf.Sqrt((oP1.x - oP2.x) * (oP1.x - oP2.x) + (oP1.y - oP2.y) * (oP1.y - oP2.y));
+            float length2 = Mathf.Sqrt((nP1.x - nP2.x) * (nP1.x - nP2.x) + (nP1.y - nP2.y) * (nP1.y - nP2.y));
+
+            return length1 < length2;
+        }
+    }
+}
